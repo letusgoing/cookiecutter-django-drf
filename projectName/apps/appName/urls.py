6193ce148a273a40a74a6d237d620587a8e9add2
zhengwei <zhengwei@dusto-yc.com>
2021-09-10 23:52:45 +0800

    update,add requirements

--- projectName/apps/appName/urls.py
+++ projectName/apps/appName/urls.py
@@ -1,4 +1,4 @@
-"""{{cookiecutter.app_name}} URL Configuration
+"""appName URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- projectName/apps/appName/wsgi.py
+++ projectName/apps/appName/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for {{cookiecutter.app_name}} project.
+WSGI config for appName project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{cookiecutter.app_name}}.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appName.settings')
 
 application = get_wsgi_application()
